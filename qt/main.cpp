f2580802c8be3543bb61d920836660d94ac5c4e1
gezawatt <gordeev-pure@yandex.ru>
2025-07-10 01:00:30 +0300

    Logging fixes

--- qt/main.cpp
+++ qt/main.cpp
@@ -89,24 +89,26 @@ public:
   public:
     InitializeFinalize() : m_debugLog(LDEBUG)
     {
+      static char const * const kLogFileName = ".\\organicmaps.log";
       // App runs without error console under win32.
-      m_errFile = ::freopen(".\\mapsme.log", "w", stderr);
+      m_errFile = ::freopen(kLogFileName, "w", stderr);
 
       // Redirecting std::cerr to a log file.
       m_cerrDefaultStreambuf = std::cerr.rdbuf();
-      m_cerrOfstream.open(".\\mapsme.log", std::ios::trunc);
-      std::cerr.rdbuf(m_cerrOfstream.rdbuf());
+      m_cerrOfstream.open(kLogFileName, std::ios::trunc);
+      if (m_cerrOfstream.is_open())
+        std::cerr.rdbuf(m_cerrOfstream.rdbuf());
 
       //_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_DELAY_FREE_MEM_DF);
       //_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     }
     ~InitializeFinalize()
     {
-      if (m_errFile)
-        ::fclose(m_errFile);
-
       if (m_cerrDefaultStreambuf)
         std::cerr.rdbuf(m_cerrDefaultStreambuf);
+
+      if (m_errFile)
+        ::fclose(m_errFile);
     }
   };
 #else
